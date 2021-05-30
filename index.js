0c0ea6ff213ee7db50dd1b5518e98fcafe8b3a4c
TomboY90 <rjsahdia3@gmail.com>
2021-05-30 19:10:52 +0900

    config mongo

--- index.js
+++ index.js
@@ -3,14 +3,36 @@ const app = express();
 const PORT = 3000;
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://admin:qwer1234@cluster0.qnqs9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+const config = require('./config/key');
+
+// Models
+const { User } = require('./models/User');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 })
 .then(() => console.log('Mongoose Connected...'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Nodemon!')
 })
+
+app.post('/user', (req, res) => {
+  const user = new User(req.body);
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+
+    return res.status(200).json({
+      success: true
+    })
+  });
+})
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
